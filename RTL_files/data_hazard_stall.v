570ca1a092d4b87d1f95122fd6b020bb9df4ce94
Raqeeb58 <raqeebansari58@gmail.com>
2025-06-05 13:28:44 +0530

    Update data_hazard_stall.v

--- RTL_files/data_hazard_stall.v
+++ RTL_files/data_hazard_stall.v
@@ -5,7 +5,7 @@ input isWb_D;
 input [31:0]instruction_M,instruction_E,instruction_D;
 output add_stall;
 
-assign add_stall = ((instruction_E[31:27] == 5'b01110) & ((instruction_E[25:22] == instruction_D[21:18]) | (instruction_E[25:22] == instruction_D[17:14])) &(isWb_D)) ? 1'b1 : 1'b0 ; 
+  assign add_stall = ((instruction_E[31:27] == 5'b01110) & ((instruction_E[25:22] == instruction_D[21:18]) | (instruction_E[25:22] == instruction_D[17:14])) &(instruction_D[31:27]!= 5'b01111)) ? 1'b1 : 1'b0 ; 
 
 
 endmodule
